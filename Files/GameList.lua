6bef7b56f01cecfc59117a137daed403f7a00fc9
NitwitMC <muhammadrhuda@outlook.com>
2024-10-06 08:42:15 +0000

    update url's

--- Files/GameList.lua
+++ Files/GameList.lua
@@ -1,6 +1,6 @@
 getgenv().Games = {
-  [142823291] = "", -- MM2 Hub
-  [17625359962] = "", --RIVALS Hub
-  [15869837051] = "", --Ball Test
-  [82584166303511] = "", --Jimmy the Normal Ball
+  [142823291] = "https://dev.azure.com/unboundedscripts/38607842-9607-43bb-86df-b5e57e61d35d/_apis/git/repositories/5fe415ef-edec-448d-8b0c-e1c72fd53257/items?path=/Games/mm2.lua", -- MM2 Hub
+  [17625359962] = "https://dev.azure.com/unboundedscripts/38607842-9607-43bb-86df-b5e57e61d35d/_apis/git/repositories/5fe415ef-edec-448d-8b0c-e1c72fd53257/items?path=/Games/rivals.lua", --RIVALS Hub
+  [15869837051] = "https://dev.azure.com/unboundedscripts/38607842-9607-43bb-86df-b5e57e61d35d/_apis/git/repositories/7e2eb34b-1dda-4c99-8e76-2ee9e189c9ff/items?path=/script.lua", --Ball Test
+  [82584166303511] = "https://dev.azure.com/unboundedscripts/38607842-9607-43bb-86df-b5e57e61d35d/_apis/git/repositories/7e2eb34b-1dda-4c99-8e76-2ee9e189c9ff/items?path=/Additional/jb.lua", --Jimmy the Normal Ball
 }
--- Files/MainLoad.lua
+++ Files/MainLoad.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("", true))()
+loadstring(game:HttpGet("https://dev.azure.com/unboundedscripts/38607842-9607-43bb-86df-b5e57e61d35d/_apis/git/repositories/5fe415ef-edec-448d-8b0c-e1c72fd53257/items?path=main.lua", true))()
