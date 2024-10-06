9a7481cc6d34112a200a6c51755b63e4e0edb50c
R H <165597973+rhuda21@users.noreply.github.com>
2024-10-06 13:57:40 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://dev.azure.com/unboundedscripts/38607842-9607-43bb-86df-b5e57e61d35d/_apis/git/repositories/5fe415ef-edec-448d-8b0c-e1c72fd53257/items?path=/Files/GameList.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UnboundedScripts/uscriptloader/refs/heads/main/Files/GameList.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
