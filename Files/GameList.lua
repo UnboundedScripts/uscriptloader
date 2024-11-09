de5f812f27fc32f214c87eae4af9e4b57ead367b
R H <165597973+rhuda21@users.noreply.github.com>
2024-11-09 18:06:15 +0000

    Update GameList.lua

--- Files/GameList.lua
+++ Files/GameList.lua
@@ -1,6 +1,6 @@
 getgenv().Games = {
   [142823291] = "https://raw.githubusercontent.com/UnboundedScripts/uscriptloader/refs/heads/main/Games/mm2.lua", -- MM2 Hub
   [17625359962] = "https://raw.githubusercontent.com/UnboundedScripts/uscriptloader/refs/heads/main/Games/rivals.lua", --RIVALS Hub
-  [15869837051] = "https://raw.githubusercontent.com/UnboundedScripts/balltest_v2/refs/heads/main/init.lua", --Ball Test
+  [15869837051] = "https://raw.githubusercontent.com/UnboundedScripts/balltest_v2/refs/heads/main/script.lua", --Ball Test
   [82584166303511] = "https://raw.githubusercontent.com/UnboundedScripts/balltest_v2/refs/heads/main/script.lua", --Jimmy the Normal Ball
 }
